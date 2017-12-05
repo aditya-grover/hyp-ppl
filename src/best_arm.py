5e1b049a36ccdcaf9e19f899ae432085e76e9509
rejuvyesh <mail@rejuvyesh.com>
2017-12-04 21:20:04 -0800

    try

--- src/best_arm.py
+++ src/best_arm.py
@@ -3,59 +3,65 @@ import tensorflow as tf
 from edward.models import *
 import argparse
 
+
 def parse_args():
 
-	parser = argparse.ArgumentParser(description='Best arm identification with partial and full delayed feedback.')
-	parser.add_argument('--seed', default=0, type=int,
-						help='Seed for random number generators')
-	parser.add_argument('--n', default=40, type=int,
-						help='Number of total arms')
-	parser.add_argument('--delta', default=0.05, type=float,
-						help='Target error')
-	parser.add_argument('--k', default=1, type=int,
-						help='Number of top arms')
+    parser = argparse.ArgumentParser(
+        description='Best arm identification with partial and full delayed feedback.')
+    parser.add_argument('--seed', default=0, type=int, help='Seed for random number generators')
+    parser.add_argument('--n', default=40, type=int, help='Number of total arms')
+    parser.add_argument('--delta', default=0.05, type=float, help='Target error')
+    parser.add_argument('--k', default=1, type=int, help='Number of top arms')
+
 
 def get_arms():
-	# create random generative processes for different arms
-	prior_means = Uniform(low=3., high=4.)
-	prior_scale = Uniform(low=0., high=2.)
+    # create random generative processes for different arms
+    prior_means = Uniform(low=3., high=4.)
+    prior_scale = Uniform(low=0., high=2.)
 
-	arms = []
-	for arm_idx in range(n):
-		arms.append(Normal(loc=prior_means.sample(), scale=prior_scale.sample()))
+    arms = []
+    for arm_idx in range(n):
+        arms.append(Normal(loc=prior_means.sample(), scale=prior_scale.sample()))
 
-	return arms
+    return arms
+
 
 def get_confidence_interval(arm, number_pulls):
 
-	return tf.abs(arm).quantile(1-delta) / tf.sqrt(number_pulls)
+    return tf.abs(arm).quantile(1 - delta) / tf.sqrt(number_pulls)
+
 
 def get_best_arm(arms):
 
-	empirical_means = tf.zeros(dims=[n], value=0, name='empirical_means')
-	pull_counts = tf.zeros(dims=[n], value=0, name='arm_counts')
-	confidence_bounds = tf.fill(dims=[n], value=np.inf, name='confidence_bounds')
-	surviving_arms = arms
+    empirical_means = tf.zeros(dims=[n], value=0, name='empirical_means')
+    pull_counts = tf.zeros(dims=[n], value=0, name='arm_counts')
+    confidence_bounds = tf.fill(dims=[n], value=np.inf, name='confidence_bounds')
+    surviving_arms = arms
 
-	while (len(surviving_arms) > 1):
-		for arm_idx, arm in enumerate(surviving_arms):
-			pull_outcome = arm.sample()
-			# update empirical means, confidence intervals
-			empirical_means[arm_idx] 
+    while (len(surviving_arms) > 1):
+        for arm_idx, arm in enumerate(surviving_arms):
+            pull_outcome = arm.sample()
+            # update empirical means, confidence intervals
+            pull_counts[arm_idx] += 1
+            empirical_means[arm_idx] = (
+                empirical_means[arm_idx] *
+                (pull_counts[arm_idx] - 1) + pull_outcome) / pull_counts[arm_idx]
 
+            confidence_bounds[arm_idx] = get_confidence_interval(empirical_means[arm_idx],
+                                                                 pull_counts[arm_idx])
+
+        maxarm = tf.argmax(empirical_means)
 
 
 if __name__ == '__main__':
 
-	args = parse_args()
-	args_dict = vars(args)
-	globals().update(args_dict)
+    args = parse_args()
+    args_dict = vars(args)
+    globals().update(args_dict)
 
-	np.random.seed(seed)
-	np.set_printoptions(threshold=np.inf)
+    np.random.seed(seed)
+    np.set_printoptions(threshold=np.inf)
 
-	arms = get_arms()
+    arms = get_arms()
 
-	get_best_arm(arms)
-
-
+    get_best_arm(arms)
